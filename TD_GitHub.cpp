ad53df22a1b9e5aaf5284396496d8d94a5e3a1a1
KylianMay <128788323+KylianMay@users.noreply.github.com>
2023-03-31 09:54:35 +0200

    Add files via upload

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,23 +1,22 @@
-//Fonctions réalisées par l'étudiant 1 :
+#include "TD_GitHub.h"
+#include <iostream>
+using namespace std;
 
 
 
 
+//Fonctions r�alis�es par l'�tudiant 2 :
+bool estPair(int nb)
+{
+	return nb % 2 == 0;
+}
 
-
-
-
-
-
-//Fonctions réalisées par l'étudiant 2 :
-
-
-
-
-
-
-
-
-
-
-//Fonctions réalisées par l'étudiant 3 :
+int sommeNombre(int nb)
+{
+	int somme = 0;
+	for (int i = 1; i <= nb; i++) 
+	{
+		somme += i;
+	}
+	return somme;
+}
